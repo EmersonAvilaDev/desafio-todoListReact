99e31c36be5de66d110684fce722b13d79e636b7
EMERSON AVILA <106774143+EmersonAvilaDev@users.noreply.github.com>
2023-09-08 20:04:09 -0300

    att: notification edit-task

--- src/view/Home/Home.jsx
+++ src/view/Home/Home.jsx
@@ -40,6 +40,16 @@ const Home = ({ data }) => {
     );
 
     setTasks(updatedTasks);
+    toast.success("Tarefa Editada com sucesso!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   };
 
   const removeTask = (id) => {
